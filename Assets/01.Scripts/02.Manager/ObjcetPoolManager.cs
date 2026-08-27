fc6a666ac445c955e065dd867443d3dbbd1d2ce6
seongwoo1246 <seongau717@gmail.com>
2026-08-28 03:53:53 +0900

    ObjectPoolManager(검사 완료)
    테스트를 통과해서 정상적으로
    소환할 때 리스트에 담아지고
    리스트에서 정상적으로 비활성화 되는 거 확인

--- Assets/01.Scripts/02.Manager/ObjcetPoolManager.cs
+++ Assets/01.Scripts/02.Manager/ObjcetPoolManager.cs
@@ -1,12 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-
-
-
-
 /// <summary>
 /// 풀링해서 사용할 오브젝트라면 반드시 이 2가지를 해야하기에 만든 인터페이스 
 /// </summary>
@@ -38,21 +32,83 @@ public class ObjcetPoolManager : MonoBehaviour
 {
     public static ObjcetPoolManager Instance {  get; private set; }
 
+    //현재는 enum을 넣어서 사용중 변경 필요시 변경예정
+    private readonly Dictionary<enumType, object> _pool = new Dictionary<enumType, object>();
+
     private void Awake()
     {
-        if(Instance == null&&Instance !=this)
+        if(Instance == null)
+        {
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
         {
             Destroy(gameObject);
-            return;
         }
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
+       
         
     }
 
+
+
+    /// <summary>
+    /// 새로운 종류의 폴을 목록에 저장해두는 함수
+    /// </summary>
+    /// <param name="enumname">이 풀에 들어갈 enum의 이름으로 풀의 이름이기도 함</param>
+    /// <param name="prefab">찍어낼 물건</param>
+    /// <param name="initialSize">미리 찍어낼 숫자</param>
+    public void RegisterPool<T>(enumType enumname, T prefab, int initialSize = 0) where T : Component, IPoolable
+    {
+        if (_pool.ContainsKey(enumname))
+        {
+            Debug.Log("이미 같은 이름의 폴이 있음");
+            return;
+        }
+
+        Transform PoolParent = new GameObject($"Pool_{enumname}").transform;
+        // PoolParent.SetParent(transform);
+
+        _pool[enumname] = new Pool<T>(prefab, PoolParent, initialSize);
+    }
+
+
+    /// <summary>
+    /// 게임에서 쓰기 위해 꺼내는 함수
+    /// </summary>
+    public T Spawn<T>(enumType enumname) where T : Component, IPoolable
+    {
+        if (!_pool.TryGetValue(enumname, out object poolObj))
+        {
+            Debug.Log($"{enumname}라는 이름표의 폴링이 없습니다. RegisterPool을 먼저 해야 합니다.");
+            return null;
+        }
+        Pool<T> pool = (Pool<T>)poolObj;
+        return pool.Get();
+    }
+
+
+    /// <summary>
+    /// 다 사용한 후 되돌리기 함수
+    /// </summary>
+    public void Despawn<T>(enumType enumname, T obj) where T : Component, IPoolable
+    {
+        if (!_pool.TryGetValue(enumname, out object poolObj))
+        {
+            Debug.Log($"{enumname}라는 이름표의 폴링이 없습니다.");
+            return;
+        }
+
+        Pool<T> pool = (Pool<T>)poolObj;
+        pool.Return(obj);
+    }
+
+
+
 }
 
- class Pool<T> where T : Component , IPoolable
+class Pool<T> where T : Component , IPoolable
 {
     // 프로젝트에 새로 만들 때 넣어줄 설계도
     private readonly T _prefab;
@@ -62,8 +118,7 @@ public class ObjcetPoolManager : MonoBehaviour
     private readonly Stack<T> _inactive = new Stack<T>();
     //지금 밖에서 사용하고 있을 물건들 담아두는 용도
     private readonly HashSet<T> _active = new HashSet<T>();
-    //현재는 enum을 넣어서 사용중 변경 필요시 변경예정
-    private readonly Dictionary<enumType, object > _pool = new Dictionary<enumType, object>();
+   
 
     public Pool(T prefab, Transform parent, int initialSize)
     {
@@ -76,6 +131,8 @@ public class ObjcetPoolManager : MonoBehaviour
             obj.gameObject.SetActive(false);
             _inactive.Push(obj);
         }
+
+
     }
 
     // 처음 만들 때 새로 만드는 함수
@@ -120,58 +177,7 @@ public class ObjcetPoolManager : MonoBehaviour
 
 
 
-    /// <summary>
-    /// 새로운 종류의 폴을 목록에 저장해두는 함수
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="enumname">이 풀에 들어갈 enum의 이름으로 풀의 이름이기도 함</param>
-    /// <param name="prefab">찍어낼 물건</param>
-    /// <param name="initialSize">미리 찍어낼 숫자</param>
-    public void RegisterPool<T>(enumType enumname ,T prefab, int initialSize =0 )where T : Component, IPoolable
-    {
-        if(_pool.ContainsKey(enumname))
-        {
-            Debug.Log("이미 같은 이름의 폴이 있음");
-            return;
-        }
-
-        Transform PoolParent = new GameObject($"Pool_{enumname}").transform;
-       // PoolParent.SetParent(transform);
-
-        _pool[enumname] = new Pool<T>(prefab, PoolParent, initialSize);
-    }
-
-
-    /// <summary>
-    /// 게임에서 쓰기 위해 꺼내는 함수
-    /// </summary>
-    public T Spawn<T>(enumType enumname) where T : Component, IPoolable
-    {
-        if( !_pool.TryGetValue(enumname,out object poolObj))
-        {
-            Debug.Log($"{enumname}라는 이름표의 폴링이 없습니다. RegisterPool을 먼저 해야 합니다.");
-            return null;
-        }
-        Pool<T> pool = (Pool<T>)poolObj;
-        return pool.Get();
-    }
-
-
-    /// <summary>
-    /// 다 사용한 후 되돌리기 함수
-    /// </summary>
-    public void Despawn<T>(enumType enumname, T obj) where T : Component, IPoolable
-    {
-        if (!_pool.TryGetValue(enumname, out object poolObj))
-        {
-            Debug.Log($"{enumname}라는 이름표의 폴링이 없습니다.");
-            return;
-        }
-
-        Pool<T> pool = ( Pool<T>)poolObj;
-        pool.Return(obj);
-    }
-
+   
 
 }
 
@@ -190,13 +196,46 @@ class 클래스이름 : MonoBehaviour , IPoolable
         사라질 때 할 것들 
     }
 }
-게임 시작시 등록할 때
+
+
+
+게임 시작시 등록할 때 (게임 매니저등에서 스타트에 넣어줘야 함)
 ObjcetPoolManager.Instance.RegisterPool(소환할 enum적어주기,소환할 프리팹, initialSize : 소환할 숫자)
 
+  void Start()
+    {
+        ObjcetPoolManager.Instance.RegisterPool(enumType.Cartoon, monster, 5);
+    }
+
 꺼낼 때 
+소환할 때 리스트(Queue)에 담아줘야 여러개 소환, 여러개 해제할 때 가능
 클래스이름 변수이름  = ObjcetPoolManager.Instance.Spawn<클래스이름>("소환할 enum적어주기")
+
+ public void 꺼내는 함수()
+    {
+        test T = ObjcetPoolManager.Instance.Spawn<test>(enumType.Cartoon);
+        if(T != null )
+        {
+            spwan.Add(T);
+        }
+
+    }
 
 집어넣을 때 
 ObjcetPoolManager.Instance.Despawn("소환할 enum적어주기",변수이름)
- 
+
+public void 집어 넣는 함수()
+    {
+        if (리스트(Queue).Count == 0) return;
+
+        int lastIndex = 리스트(Queue).Count - 1;
+        test targetObj = 리스트(Queue)[lastIndex];
+        
+        ObjcetPoolManager.Instance.Despawn(enumType.Cartoon, targetObj);
+        spwan.RemoveAt(lastIndex);
+
+
+
+    } 
+
  */
