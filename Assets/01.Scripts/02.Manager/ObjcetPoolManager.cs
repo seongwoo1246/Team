153fea50fe5c9208513d0e0f852f6d25c4e17c5d
seongwoo1246 <seongau717@gmail.com>
2026-08-28 11:39:35 +0900

    오브젝트 폴링 주석 한글화 작업 완료

--- Assets/01.Scripts/02.Manager/ObjcetPoolManager.cs
+++ Assets/01.Scripts/02.Manager/ObjcetPoolManager.cs
@@ -14,7 +14,7 @@ public interface IPoolable
 }
 
 /// <summary>
-/// ������Ʈ ���� �Ҷ� ����� enum�� ���⿡ ���� ���� �� �ʿ�� ���� ����
+///폴링 할때 사용할 키값들 불러올 때 자신이 불러 오는 종류에 맞게 가져가 사용
 /// </summary>
 public enum enumType
 {
@@ -27,13 +27,13 @@ public enum enumType
 
 
 /// <summary>
-/// ��� ������Ʈ Ǯ���� ���õ� �Լ��� ���⼭ �� �ذ� �� Ŭ����
+/// 실질적으로 사용할 오브젝트 폴링 매니저 클래스
 /// </summary>
 public class ObjcetPoolManager : MonoBehaviour
 {
     public static ObjcetPoolManager Instance {  get; private set; }
 
-    //����� enum�� �־ ����� ���� �ʿ�� ���濹��
+   
     private readonly Dictionary<enumType, object> _pool = new Dictionary<enumType, object>();
 
     private void Awake()
@@ -54,12 +54,12 @@ public class ObjcetPoolManager : MonoBehaviour
 
 
 
-    /// <summary>
-    /// ���ο� ������ ���� ��Ͽ� �����صδ� �Լ�
-    /// </summary>
-    /// <param name="enumname">�� Ǯ�� �� enum�� �̸����� Ǯ�� �̸��̱⵵ ��</param>
-    /// <param name="prefab">�� ����</param>
-    /// <param name="initialSize">�̸� �� ����</param>
+   /// <summary>
+   /// 게임 매니저가 가장 먼저 해줘야 하는 내용들 (이걸 안하면 오브젝트 폴링을 사용할 수 없음)
+   /// </summary>
+   /// <param name="enumname">자신이 불러올 enum 종류</param>
+   /// <param name="prefab">자신이 만들 오브젝트</param>
+   /// <param name="initialSize">자신이 만들 갯수 </param>
     public void RegisterPool<T>(enumType enumname, T prefab, int initialSize = 0) where T : Component, IPoolable
     {
         if (_pool.ContainsKey(enumname))
@@ -76,7 +76,7 @@ public class ObjcetPoolManager : MonoBehaviour
 
 
     /// <summary>
-    /// ���ӿ��� ���� ���� ������ �Լ�
+    /// 실질적으로 소환할 때 사용할 함수
     /// </summary>
     public T Spawn<T>(enumType enumname) where T : Component, IPoolable
     {
@@ -91,7 +91,7 @@ public class ObjcetPoolManager : MonoBehaviour
 
 
     /// <summary>
-    /// �� ����� �� �ǵ����� �Լ�
+    ///실질적으로 비활성화 시킬 때 사용할 함수
     /// </summary>
     public void Despawn<T>(enumType enumname, T obj) where T : Component, IPoolable
     {
@@ -111,13 +111,13 @@ public class ObjcetPoolManager : MonoBehaviour
 
 class Pool<T> where T : Component , IPoolable
 {
-    // ������Ʈ�� ���� ���� �� �־��� ���赵
+    // 소환 원본
     private readonly T _prefab;
-    // ������Ʈ���� �����ص� �θ� ������Ʈ ��ġ
+    //소환 위치
     private readonly Transform _parent;
-    // �ȿ��� �׾Ƶΰ� ���� â���
+    // 오브젝트 폴링 담아둘 콜렉션
     private readonly Stack<T> _inactive = new Stack<T>();
-    //���� �ۿ��� ����ϰ� ���� ���ǵ� ��Ƶδ� �뵵
+    //현재 활동 중인 폴링 소환물들
     private readonly HashSet<T> _active = new HashSet<T>();
    
 
@@ -136,17 +136,14 @@ class Pool<T> where T : Component , IPoolable
 
     }
 
-    // ó�� ���� �� ���� ����� �Լ�
+    // 처음에 없을 시 오브젝트 풀링을 만듬
     private T CreateNew()
     {
         return Object.Instantiate(_prefab, _parent);
     }
 
 
-   /// <summary>
-   /// ������Ʈ ������ ����� �� �� �Լ�(������ ���� ������ ���)
-   /// </summary>
-   /// <returns></returns>
+ 
     public T Get()
     {
         T obj = _inactive.Count > 0 ? _inactive.Pop() : CreateNew();
@@ -158,10 +155,7 @@ class Pool<T> where T : Component , IPoolable
 
 
 
-    /// <summary>
-    /// �ߺ� ������ �� ��� �����ϰ� ��� �� ��Ͽ��� ������ ��Ȱ��ȭ �ϱ�
-    /// </summary>
-    /// <param name="obj"></param>
+
     public void Return(T obj)
     {
         if(!_active.Contains(obj))
@@ -183,36 +177,36 @@ class Pool<T> where T : Component , IPoolable
 }
 
 /*
- ����� ���� ����
+ 사용방법 예시
 
-1. �����Ҷ��� ������ �������̽� �־��ֱ�
-class Ŭ�����̸� : MonoBehaviour , IPoolable
+1. 인터페이스 적용
+class 클래스 이름 : MonoBehaviour , IPoolable
 {
     public void OnSpawn()
     {
-        ������ �� �� �͵� 
+        소환할 때 해야 할 일들
     }
     public void OnDespawn()
     {
-        ����� �� �� �͵� 
+        비활성화 할 때 할 일들
     }
 }
 
 
 
-���� ���۽� ����� �� (���� �Ŵ������ ��ŸƮ�� �־���� ��)
-ObjcetPoolManager.Instance.RegisterPool(��ȯ�� enum�����ֱ�,��ȯ�� ������, initialSize : ��ȯ�� ����)
+2. RegisterPool을 게임 매니저에서 실행
+ObjcetPoolManager.Instance.RegisterPool(소환할 enum종류,소환할 오브젝트, initialSize : 소환할 갯수)
 
   void Start()
     {
         ObjcetPoolManager.Instance.RegisterPool(enumType.Cartoon, monster, 5);
     }
 
-���� �� 
-��ȯ�� �� ����Ʈ(Queue)�� ������ ������ ��ȯ, ������ ������ �� ����
-Ŭ�����̸� �����̸�  = ObjcetPoolManager.Instance.Spawn<Ŭ�����̸�>("��ȯ�� enum�����ֱ�")
+꺼낼 때
+이거를 사용 할 때는 미리 List혹은 Queue, Stack등으로 미리 만들어두고 담아써야 여러개 가능
+소환클래스 변수명  = ObjcetPoolManager.Instance.Spawn<소환클래스>("소환할 enum종류")
 
- public void ������ �Լ�()
+ public void 소환함수()
     {
         test T = ObjcetPoolManager.Instance.Spawn<test>(enumType.Cartoon);
         if(T != null )
@@ -222,15 +216,15 @@ ObjcetPoolManager.Instance.RegisterPool(��ȯ�� enum�����ֱ�,
 
     }
 
-������� �� 
-ObjcetPoolManager.Instance.Despawn("��ȯ�� enum�����ֱ�",�����̸�)
+집어 넣을 때
+ObjcetPoolManager.Instance.Despawn("소환클래스",변수명)
 
-public void ���� �ִ� �Լ�()
+public void 비활성화 함수()
     {
         if (����Ʈ(Queue).Count == 0) return;
 
-        int lastIndex = ����Ʈ(Queue).Count - 1;
-        test targetObj = ����Ʈ(Queue)[lastIndex];
+        int lastIndex = List(Queue).Count - 1;
+        test targetObj = List(Queue)[lastIndex];
         
         ObjcetPoolManager.Instance.Despawn(enumType.Cartoon, targetObj);
         spwan.RemoveAt(lastIndex);
