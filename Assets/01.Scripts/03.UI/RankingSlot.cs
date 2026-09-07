c4dfd1fd4d65fcb1c603c85a96df49677d731d17
seongwoo1246 <seongau717@gmail.com>
2026-09-07 14:51:08 +0900

    오브젝트 풀링 enum 수정, 랭킹표 작성
    오브젝트 풀링은 enum을 좀 더 세분화해서 만들었고
    
    랭킹표는 현재 가장 큰 데미지, 가장 오래동안 지속한 플래이 시간, 가장 빨리 보스 잡은 클리어 시간으로 탑 3 나눌 예정

---
+++ Assets/01.Scripts/03.UI/RankingSlot.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum RankColor
+{
+    Gold,
+   Silver,
+   Bronze
+}
+
+/// <summary>
+/// 단일 랭킹 슬롯을 담당 (1,2,3위를 표시할 예정)
+/// </summary>
+public class RankingSlot : MonoBehaviour
+{
+   
+    // 랭킹을 나타낸 텍스트
+    [SerializeField] private TextMeshProUGUI RankText;
+    // 뒤에 색상이 바뀔 이미지
+    [SerializeField] private Image BackGround;
+
+    //랭킹표 1,2,3위에 따라서 이미지 색상 변경
+    double[] DamageRank = new double[2];
+
+    double[] PlayTimeRank = new double[2];
+
+    int[] ClearTimeRank = new int[2];
+
+    public void SetupSlot(int rank,double score, RankColor color)
+    {
+
+    }
+
+    public void RankColorChange(RankColor color)
+    {
+        switch(color)
+        {
+            case RankColor.Gold: BackGround.color = Color.gold; break;
+
+            case RankColor.Silver: BackGround.color = Color.silver; break;
+
+            case RankColor.Bronze: BackGround.color = Color.brown; break;
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+}
---
+++ Assets/01.Scripts/03.UI/RankingUi.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+
+/// <summary>
+/// 개인 랭킹표에 맞춰 개인 기록에 따른 랭크표
+/// </summary>
+public class RankingUi : MonoBehaviour
+{
+
+   
+
+
+   //역대 데미지 탑 3 (가장 강한 치명타 같은 느낌)
+
+    //역대 플레이 시간 탑3 (한 번 플레이 할때 지속 시간)
+
+    //역대 보스 클리어 시간 탑3( 보스를 얼마나 빨리 잡는가)
+
+
+    
+}
